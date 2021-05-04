02e23a8a9efc31f7006e9dfa66122c971a07fe19
Uri Dvir <uri.j.dvir@gmail.com>
2021-05-04 18:36:40 -0400

    Temporary fix for lack of stackpanes in MapController.java

--- src/main/java/edu/wpi/teamname/views/Mapping/MapController.java
+++ src/main/java/edu/wpi/teamname/views/Mapping/MapController.java
@@ -1584,6 +1584,7 @@ public class MapController implements AllAccessible {
   }
 
   private void resetCSV() {
+    /* ------------- OLD VERSION WITH DIALOGS ---------------------------
     if (resetOpen) return;
     resetOpen = true;
 
@@ -1633,6 +1634,13 @@ public class MapController implements AllAccessible {
     layout.setActions(subBtn, canBtn);
 
     submitDia.show();
+     */
+
+    FDatabaseTables.getNodeTable().clearTable(GlobalDb.getConnection(), "Nodes");
+    FDatabaseTables.getEdgeTable().clearTable(GlobalDb.getConnection(), "Edges");
+    FDatabaseTables.getNodeTable().populateTable(GlobalDb.getConnection(), "");
+    FDatabaseTables.getEdgeTable().populateTable(GlobalDb.getConnection(), "");
+    updateMapFromDB();
   }
 
   private void importCSV() {
