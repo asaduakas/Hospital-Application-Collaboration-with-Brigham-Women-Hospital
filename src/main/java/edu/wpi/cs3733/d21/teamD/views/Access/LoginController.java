616a6f8c9a8979bbad003a1eeee29bb321c999cc
wyy-47 <wuyiyi48@gmail.com>
2021-05-07 04:31:23 -0400

    it is what it is

--- src/main/java/edu/wpi/cs3733/d21/teamD/views/Access/LoginController.java
+++ src/main/java/edu/wpi/cs3733/d21/teamD/views/Access/LoginController.java
@@ -9,38 +9,51 @@ import edu.wpi.cs3733.d21.teamD.views.DialogFactory;
 import edu.wpi.cs3733.d21.teamD.views.SceneSizeChangeListener;
 import java.io.IOException;
 import java.util.List;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
 public class LoginController implements AllAccessible {
 
   @FXML Button exitButton;
-  @FXML Button loginButton;
+  @FXML JFXButton loginButton;
   @FXML JFXTextField usernameField;
   @FXML JFXPasswordField passwordField;
   @FXML StackPane loginStackPane;
+  @FXML AnchorPane anchor;
   public static String userCategory;
 
   private DialogFactory dialogFactory;
 
   @FXML
   public void initialize() {
+    final BooleanProperty focused = new SimpleBooleanProperty();
     dialogFactory = new DialogFactory(loginStackPane);
     loginStackPane.setPickOnBounds(false);
+
+    anchor.addEventHandler(
+        KeyEvent.KEY_PRESSED,
+        e -> {
+          if (e.getCode() == KeyCode.ENTER) {
+            loginButton.fire();
+            e.consume();
+          }
+        });
   }
 
   @FXML
   private void login(ActionEvent event) throws IOException {
+
     if (usernameField.getText().isEmpty() || passwordField.getText().isEmpty()) {
       popupWarning(event, "Please fill out the required fields.");
     } else if (!FDatabaseTables.getUserTable()
@@ -91,16 +104,20 @@ public class LoginController implements AllAccessible {
 
   public static void changeChildrenHomePage(List<Node> nodeList) {
 
+    ImageView hospitalImage = (ImageView) nodeList.get(1);
+    VBox buttons = (VBox) nodeList.get(2);
     HBox topButtons = (HBox) nodeList.get(4);
+    JFXButton logoutButton = (JFXButton) nodeList.get(5);
+    JFXButton exitButton = (JFXButton) nodeList.get(6);
+    StackPane stackPane = (StackPane) nodeList.get(7);
+
     topButtons.setLayoutX(
         App.getPrimaryStage().getScene().getWidth() - (topButtons.getWidth() + 26));
 
-    VBox buttons = (VBox) nodeList.get(2);
     buttons.setLayoutX((App.getPrimaryStage().getScene().getWidth() - buttons.getWidth()) / 2);
     buttons.setLayoutY(
         (App.getPrimaryStage().getScene().getHeight() - (buttons.getHeight() / 3)) / 2);
 
-    ImageView hospitalImage = (ImageView) nodeList.get(1);
     double newHospitalRatio;
     newHospitalRatio = 1499 / 722;
     hospitalImage.setFitHeight(App.getPrimaryStage().getScene().getHeight() - 87);
@@ -108,8 +125,6 @@ public class LoginController implements AllAccessible {
     hospitalImage.setLayoutX(
         (App.getPrimaryStage().getScene().getWidth() - hospitalImage.getFitWidth()) / 2);
 
-    JFXButton logoutButton = (JFXButton) nodeList.get(5);
-    JFXButton exitButton = (JFXButton) nodeList.get(6);
     if (hospitalImage.getFitWidth() < App.getPrimaryStage().getScene().getWindow().getWidth()) {
       logoutButton.setLayoutX(
           ((App.getPrimaryStage().getScene().getWindow().getWidth() - hospitalImage.getFitWidth())
@@ -135,6 +150,14 @@ public class LoginController implements AllAccessible {
     Text userType = (Text) nodeList.get(nodeList.size() - 1);
     userType.setX(exitButton.getLayoutX() - 170);
     userType.setY(App.getPrimaryStage().getScene().getHeight() - 25);
+
+    stackPane.setPrefWidth(500);
+    stackPane.setPrefHeight(300);
+    stackPane.setLayoutX(
+        (App.getPrimaryStage().getScene().getWidth() - stackPane.getPrefWidth()) / 2);
+    stackPane.setLayoutY(
+        (App.getPrimaryStage().getScene().getHeight() - stackPane.getHeight()) / 2);
+    stackPane.setPickOnBounds(false);
   }
 
   @FXML
